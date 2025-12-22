bc42ac25a51738eae2dbdb28c0471f8662a90f9e
Nathan Henrie <nate@n8henrie.com>
2025-12-22 09:34:59 -0700

    Flash with cargo instead of avrdude directly

--- flash.sh
+++ flash.sh
@@ -4,12 +4,6 @@ set -Eeuf -o pipefail
 set -x
 
 main() {
-  nix develop --command cargo build --release
-  nix develop --command \
-    avrdude \
-    --programmer usbasp \
-    --part attiny85 \
-    --erase \
-    --memory flash:w:target/avr-none/release/relay.elf:e
+  nix develop --command cargo run --release
 }
 main "$@"
