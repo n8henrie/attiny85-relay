a0dbdbcffd7de2e69b48f59fbaa746dca6909ade
Nathan Henrie <nate@n8henrie.com>
2026-02-09 06:10:13 -0700

    Enable building with nix

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
       rust-overlay,
     }:
     let
-      name = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.name;
+      inherit ((fromTOML (builtins.readFile ./Cargo.toml)).package) name;
       systems = [
         "x86_64-darwin"
         "aarch64-darwin"
@@ -36,36 +36,70 @@
           overlays = [ (import rust-overlay) ];
         };
         toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        rustPlatform = pkgs.makeRustPlatform {
-          rustc = toolchain;
-          cargo = toolchain;
-        };
+        rustPlatform =
+          let
+            pkgsCross = import nixpkgs {
+              inherit system;
+              crossSystem = {
+                inherit system;
+                rust.rustcTarget = "avr-none";
+              };
+            };
+          in
+          pkgsCross.makeRustPlatform {
+            rustc = toolchain;
+            cargo = toolchain;
+          };
       in
       {
         packages = {
           default = self.packages.${system}.${name};
           ${name} = rustPlatform.buildRustPackage {
             inherit name;
-            version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.version;
-            src = ./.;
-            cargoLock.lockFile = ./Cargo.lock;
+            inherit ((fromTOML (builtins.readFile ./Cargo.toml)).package) version;
+            src = pkgs.lib.cleanSource ./.;
+            env.RUSTFLAGS =
+              let
+                inherit (pkgs) lib pkgsCross;
+              in
+              lib.concatStringsSep " " [
+                "-C linker=${lib.getExe pkgsCross.avr.buildPackages.gcc}"
+                "-C target-cpu=attiny85"
+              ];
+            nativeBuildInputs = with pkgs; [ pkgsCross.avr.gcc ];
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              outputHashes."attiny-hal-0.1.0" = "sha256-dT4ClJC2eysro92JSuLVGRINGzgkxKZQjBad/UxVDd0=";
+            };
+            meta.mainProgram = "relay.elf";
           };
         };
 
-        apps.default = {
-          type = "app";
-          program = "${self.packages.${system}.${name}}/bin/${name}";
+        apps = {
+          default = self.outputs.apps.${system}.flash;
+          flash = {
+            type = "app";
+            program =
+              let
+                inherit (pkgs) lib;
+                flasher = pkgs.writeShellApplication {
+                  name = "ravedude-with-avr-gcc";
+                  runtimeInputs = with pkgs; [
+                    pkgsCross.avr.buildPackages.gcc
+                    ravedude
+                  ];
+                  text = "ravedude ${lib.getExe self.outputs.packages.${system}.${name}}";
+                };
+              in
+              lib.getExe flasher;
+          };
         };
 
         devShells.default = pkgs.mkShell {
-          buildInputs = [
-            toolchain
-          ]
-          ++ (with pkgs; [
-            avrdude
-            pkgsCross.avr.buildPackages.gcc
-            ravedude
-          ]);
+          inputsFrom = [
+            self.outputs.packages.${system}.${name}
+            self.outputs.apps.${system}.default
+          ];
         };
       }
     );
