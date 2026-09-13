f1a989eacee056bd21a64bb7be4814e1d3cebb5a
Nathan Henrie <nate@n8henrie.com>
2026-09-13 12:30:20 -0600

    Don't forget to pass gcc in from default.nix

--- default.nix
+++ default.nix
@@ -2,5 +2,6 @@ let
   pkgs = import <nixpkgs> { };
 in
 pkgs.callPackage ./package.nix {
+  avrGcc = pkgs.pkgsCross.avr.buildPackages.gcc;
   rustPlatform = pkgs.callPackage ./rustplatform-with-src.nix { };
 }
